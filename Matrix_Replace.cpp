63dd8c974c0aafe8c0cdc261d9542a4593d47e43
RodixJony <legendjony02@gmail.com>
2022-10-22 10:29:31 +0600

    Matix Replace

---
+++ Matrix_Replace.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int r,c;
+    cin>>r>>c;
+    int a[r][c],b[r][c],mul[r][c];
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            cin>>a[i][j];
+        }
+    }
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            cin>>b[i][j];
+        }
+    }
+
+
+
+    return 0;
+}
